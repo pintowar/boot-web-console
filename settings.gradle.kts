b29107217601be4844b4ad05dc491cb478c5cba2
Thiago Oliveira <pintowar@gmail.com>
2023-04-13 22:12:46 -0300

    changing sample project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,4 +3,4 @@ rootProject.name = "jweb-console"
 include("jweb-console-api", "jweb-console-webcli")
 include("jweb-console-groovy", "jweb-console-jruby")
 include("jweb-console-boot")
-include("samples:boot-groovy-console")
+include("samples:boot-console")
