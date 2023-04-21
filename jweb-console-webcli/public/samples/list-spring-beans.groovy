7aa4e18ab3b9f1eb9333855278076f04aec34b3f
Thiago Oliveira <pintowar@gmail.com>
2023-04-21 12:33:11 -0300

    adjusting webcli script samples

--- jweb-console-webcli/public/samples/list-spring-beans.groovy
+++ jweb-console-webcli/public/samples/list-spring-beans.groovy
@@ -1,2 +1,4 @@
 // list all spring boot beans (must adapt to work on other frameworks)
-applicationContext.getBeanDefinitionNames()
+applicationContext.getBeanDefinitionNames().sort().each {
+    println it
+}
