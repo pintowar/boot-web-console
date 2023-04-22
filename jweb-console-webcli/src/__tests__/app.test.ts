3be1cef850c58d64651dd84017970439e54e1cdb
Thiago Oliveira <pintowar@gmail.com>
2023-04-21 21:03:37 -0300

    adding more webcli component test

---
+++ jweb-console-webcli/src/__tests__/app.test.ts
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/svelte";
+import userEvent from '@testing-library/user-event'
+import App from "../App.svelte";
+
+describe("App.svelte", () => {
+    it("should render initial state", () => {
+        const { container } = render(App);
+        expect(container).toBeTruthy();
+    });
+});
